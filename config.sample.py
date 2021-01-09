640271a812d7da26f787a3771d89efc3b04d6116
FireRedz <fireredz69420@gmail.com>
2021-01-09 19:51:45 +0800

    config mmmm

---
+++ config.sample.py
@@ -0,0 +1,13 @@
+
+## MAIN
+server_name = 'Yuzumi' # yes
+domain = 'cock.redz.tk' # change this to your domain
+passwd_salt = '69420GAMERINGGAMER' # not used atm
+
+## CRON
+# 1 = 1 Minute
+cron_delay = 1  # used for updating user stats
+
+## RANKING
+pp = False # enable this to use pp system (currently using osu! pp system + TD mod... might use Alice's DroidPP)
+osu_key = 'YOURAPIKEYHERE' # used for beatmap info, unused if pp is disabled
