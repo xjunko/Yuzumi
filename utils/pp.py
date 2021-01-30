51566c6f771ce8a91ae8b580c3f3affeb0227d09
FireRedz <fireredz69420@gmail.com>
2021-01-30 18:53:23 +0800

    local pp calculator

--- utils/pp.py
+++ utils/pp.py
@@ -9,12 +9,14 @@ import asyncio
 import aiofiles
 import aiohttp
 import json
-#from pathlib import Path
+import logging
+import oppadc
+from pathlib import Path
 from enum import Enum, IntEnum
 
 from objects import glob
 
-#beatmap_path = Path.cwd() / 'data/'
+beatmap_path = Path.cwd() / 'data/beatmaps'
 
 class droidMods(Enum):
     nm = '-'
@@ -66,8 +68,8 @@ class PPCalculator:
         local pp calculation
 
     """
-    def __init__(self, map_id:int, **kwargs):
-            self.map_id  = map_id
+    def __init__(self, path: Path, **kwargs):
+            self.bm_path = path
 
             self.mods = modsBitsFromDroidStr(kwargs.get('mods', '-'))
             self.combo = kwargs.get('combo', 0)
@@ -96,47 +98,46 @@ class PPCalculator:
 
                     return data['beatmap_id']
 
+    @classmethod
+    async def file_from_osu(cls, md5: str):
+        if not (bm_id := await cls.data_from_osuapi(md5)):
+            return logging.error(f"Failed to get map data from api md5: {md5}")
+
+
+        url = f'https://old.ppy.sh/osu/{bm_id}'
+        path = beatmap_path / f'{bm_id}.osu'
+
+        if path.exists():
+            return path
+
+        async with aiohttp.ClientSession() as sess:
+            async with sess.get(url) as res:
+                if res.status != 200:
+                    logging.error(f"Failed to get map {bm_id}")
+                    return None
+
+                content = await res.read()
+
+        path.write_bytes(content)
+        return path
+
 
 
 
     @classmethod
     async def from_md5(cls, md5: str, **kwargs):
-        if not (res := await cls.data_from_osuapi(md5)):
-            return 0
-
-        # return 0 if pp system is disabled
-        if not glob.config.pp:
-            return 0
+        if not (res := await cls.file_from_osu(md5)) or not glob.config.pp:
+            return False
 
         return cls(res, **kwargs)
 
 
 
     async def calc(self):
-        ''' call cyperdark api for pp, will rewrite to use .osu file instead '''
-        params = {}
-        params['id'] = self.map_id
+        curMap = oppadc.OsuMap(file_path=self.bm_path)
+        pp = curMap.getPP(self.mods, accuracy=self.acc, misses=self.nmiss, combo=self.combo)
 
-        if self.mods:
-            params['mods'] = int(self.mods)
-        if self.combo:
-            params['combo'] = int(self.combo)
-        if self.nmiss:
-            params['miss'] = int(self.nmiss)
-        if self.acc:
-            params['acc'] = int(self.acc)
-
-
-
-        url = 'https://pp.osuck.net/pp'
-
-        async with aiohttp.ClientSession() as sess:
-            async with sess.get(url, params=params) as res:
-                if res and res.status == 200:
-                    data = await res.json()
-                    return data['pp']['current']
-
-        return 0.0
+        return pp.total_pp
 
 
 
@@ -149,7 +150,6 @@ async def recalc_scores():
     scores = await glob.db.fetchall('select * from scores where status = 2')
     for score in scores:
         m = await PPCalculator.from_md5(score['mapHash'])
-
         if m:
             m.acc = score['acc']
             m.nmiss = score['hitmiss']
@@ -159,7 +159,7 @@ async def recalc_scores():
 
             pp = await m.calc()
 
-            await glob.db.execute('update scores set pp = ? where id = ?', [pp, score['id']])
-            print(f'finished calculating {score["id"]} => pp: {pp}')
-        else:
-            print(f'failed calculating {score["id"]}')
+            print(score['mapHash'], pp)
+
+
+
