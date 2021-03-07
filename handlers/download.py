ccadfcca64e7ca2d433e797bc456b320008fa79d
FireRedz <fireredz69420@gmail.com>
2021-03-08 02:24:43 +0800

    cleanup

--- handlers/download.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-import os
-
-
-async def download_apk(request):
-    if not os.path.isfile('data/release/latest.apk'):
-        return web.Response(text='not yet lol')
-
-    return web.FileResponse('data/release/latest.apk')
