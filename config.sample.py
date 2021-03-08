4cc7422439f18b856e2b6bc8197908bde4d32364
FireRedz <fireredz69420@gmail.com>
2021-03-09 02:35:10 +0800

    Removed domain

--- config.sample.py
+++ config.sample.py
@@ -3,7 +3,6 @@
 server_name = 'Yuzumi'       # yes
 port = 80                    # server port
 host = '0.0.0.0'             #
-domain = 'your-domain.here'  # change this to your domain
 
 ## CRON ##
 # 1 = 1 Minute
@@ -13,11 +12,9 @@ cron_delay = 1  # used for updating user stats
 #- GLOBAL -#
 # enable to use pp system
 pp = False
+pp_leaderboard = False # Shows pp instead of score and sort by pp
 # used for beatmap info, unused if pp is disabled
 osu_key = 'YOURAPIKEYHERE'
-
-#- MAP LEADERBOARD -#
-pp_leaderboard = False # Shows pp instead of score and sort by pp
 
 #- MAINTENACE -#
 disable_submit = False # Does what it does, shows a message to user when trying to play submit a play.
