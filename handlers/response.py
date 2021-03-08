4cec0470c464e61f070e34961ae10e9c70461091
FireRedz <fireredz69420@gmail.com>
2021-03-09 01:21:33 +0800

    Add: Added Failure to responses

--- handlers/response.py
+++ handlers/response.py
@@ -2,8 +2,10 @@ def args_join(args):
   return ' '.join([str(x) for x in args])
 
 def Success(*args: list):
-  return f'SUCCESS\n{args_join(args)}'
+  return 'SUCCESS\n' + args_join(args)
 
 def Failed(*args: list):
-  return f'FAILED\n{args_join(args)}'
+  return 'FAILED\n' + args_join(args)
 
+def Failure(*args: list):
+  return 'FAILURE\n' + args_join(args)
